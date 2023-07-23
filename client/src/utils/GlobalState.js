9454a61f1c0b34e8581242b76116493c9e89bf7e
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-23 15:03:37 +1000

    changed yp GlobaState.js so that the store is seperate

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from 'redux';
 import reducer from './reducers';
-
-// Create the Redux store using the reducer and initial state
-const store = createStore(reducer);
+import store from './store';
 
 // Create a provider component to wrap your app and provide access to the store
-const StoreProvider = (props) => {
-  return <Provider store={store}{...props} />;
+export default function StoreProvider(props) {
+  return <Provider store={store} {...props} />;
 };
 
-export default StoreProvider;
 
